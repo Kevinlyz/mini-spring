20d4627fa016c3fac97474d96314f6231f442316
kevin <hbukevin@gmail.com>
2019-06-05 09:28:05 +0800

    提交starter

---
+++ framework/src/main/java/com/qcby/starter/MiniApplication.java
@@ -0,0 +1,13 @@
+package com.qcby.starter;
+
+/**
+ * @author kevinlyz
+ * @ClassName MiniApplication
+ * @Description 框架的入口类
+ * @Date 2019-06-04 19:21
+ **/
+public class MiniApplication {
+    public static void run(Class<?> cls,String[] args){
+        System.out.println("Hello mini-spring application！");
+    }
+}
---
+++ test/src/main/java/com/qcby/Application.java
@@ -0,0 +1,16 @@
+package com.qcby;
+
+import com.qcby.starter.MiniApplication;
+
+/**
+ * @author kevinlyz
+ * @ClassName Application
+ * @Description TODO
+ * @Date 2019-06-04 19:02
+ **/
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        MiniApplication.run(Application.class,args);
+    }
+}
