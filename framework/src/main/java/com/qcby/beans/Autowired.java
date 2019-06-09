e0fe7be61200226747361cc3f3c3e1d1eed73f84
kevin <hbukevin@gmail.com>
2019-06-09 16:31:43 +0800

    实现依赖注入

---
+++ framework/src/main/java/com/qcby/beans/Autowired.java
@@ -0,0 +1,9 @@
+package com.qcby.beans;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Autowired {
+}
---
+++ framework/src/main/java/com/qcby/beans/Bean.java
@@ -0,0 +1,9 @@
+package com.qcby.beans;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Bean {
+}
